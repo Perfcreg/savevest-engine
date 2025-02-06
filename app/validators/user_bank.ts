5973154e9f46b24cab243ed5e10486aa5757b85b
Perfcreg <perfcreg@gmail.com>
2025-02-06 09:37:56 +0100

    remove sensitive files and add env example

--- app/validators/user_bank.ts
+++
@@ -1,9 +0,0 @@
-import vine from '@vinejs/vine'
-
-export const bankValidator = vine.compile(
-    vine.object({
-      bank_code: vine.string(),
-      account_number: vine.string(),
-      bank_name: vine.string(),
-    })
-  );
--- start/routes/auth.ts
+++
@@ -1,9 +0,0 @@
-import router from '@adonisjs/core/services/router'
-import { middleware } from '../kernel.js'
-
-router.group(() => {
-  router.post('verify-2fa', '#controllers/two_factor_controller.verify')
-  router.post('2fa/toggle', '#controllers/two_factor_controller.toggle').use(
-    middleware.auth()
-  )
-}).prefix('/api/auth')
